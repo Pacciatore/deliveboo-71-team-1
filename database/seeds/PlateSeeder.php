77be1710e62e4975f0a9cf66824017bbc06df4d9
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-05 16:56:58 +0100

    plateSeeder with faker

--- database/seeds/PlateSeeder.php
+++ database/seeds/PlateSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Plate;
+use Faker\Generator as Faker;
 
 class PlateSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class PlateSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for ($i = 0; $i < 10; $i++) {
+            $plate = new Plate();
+
+            $plate->name = $faker->word() . $i;
+            $plate->description = $faker->text();
+            $plate->price = $faker->randomFloat(2, 0, 99);
+
+            $plate->save();
+        }
     }
 }
