91053b5a5084c3bc0f3ac4716401727118c42998
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-06 13:32:14 +0100

    cambio grafico index

--- resources/views/admin/plates/index.blade.php
+++ resources/views/admin/plates/index.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="container">
-        <h1>Elenco piatti</h1>
+    <div id="plates-index" class="container">
 
-        <a href="{{ route('admin.plates.create') }}">Aggiungi piatto</a>
+        <div class="top d-flex justify-content-between">
 
-        @foreach ($plates as $plate)
-            <p> <a href="{{ route('admin.plates.show', $plate->id) }}"> {{ $plate->name }}</a></p>
-        @endforeach
+            <h1>Elenco piatti</h1>
+
+            <a class="btn boo-btn-orange d-flex align-items-center" href="{{ route('admin.plates.create') }}">Aggiungi
+                piatto</a>
+
+        </div>
+
+        <div class="mt-4">
+            @foreach ($plates as $plate)
+                <p> <a href="{{ route('admin.plates.show', $plate->id) }}"> {{ $plate->name }}</a></p>
+            @endforeach
+        </div>
+
     </div>
 @endsection
