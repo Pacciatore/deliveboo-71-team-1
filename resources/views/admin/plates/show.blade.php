f66b005ba734f03e235cbb53067b23ee29aa48aa
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-06 14:57:40 +0100

    stile show plate

--- resources/views/admin/plates/show.blade.php
+++ resources/views/admin/plates/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="container">
+    <div id="show-plate" class="container">
         {{-- Nome del piatto --}}
         <h1>{{ $plate->name }}</h1>
 
@@ -13,7 +13,7 @@
         @endif
 
         {{-- Descrizione/Ingredienti --}}
-        <div class="plate-info">
+        <div class="plate-info my-3">
             <h4>Descrizione: </h4>
             <p>{{ $plate->description }}</p>
         </div>
@@ -23,24 +23,28 @@
 
         {{-- Disponibilità piatto --}}
         @if ($plate->available)
-            <p>Piatto disponibile</p>
+            <p class="available">Piatto disponibile</p>
         @else
-            <p>Piatto non disponibile</p>
+            <p class="not-available">Piatto non disponibile</p>
         @endif
 
 
-        <div class="nav-buttons">
-            <a href="{{ route('admin.plates.index') }}">Back to index</a>
-            <a href="{{ route('admin.plates.edit', $plate->id) }}">Edit</a>
+        <div class="nav-buttons d-flex align-items-center justify-content-end g-2">
+
+            <a class="btn boo-btn-cyan d-flex align-items-center" href="{{ route('admin.plates.index') }}">Back to
+                index</a>
+
+            <a class="btn boo-btn-green d-flex align-items-center mx-2"
+                href="{{ route('admin.plates.edit', $plate->id) }}">Edit</a>
 
             {{-- Pulsante per l'eliminazione di un piatto --}}
-            <div class="mt-2">
+            <div>
                 <form onsubmit="return confirm('Are you sure?')" action="{{ route('admin.plates.destroy', $plate->id) }}"
                     method="POST">
                     @csrf
                     @method('DELETE')
 
-                    <input class="btn-danger" type="submit" value="Delete Plate">
+                    <input class="btn btn-danger" type="submit" value="Delete Plate">
                 </form>
             </div>
 
