fa520f6e22886739313aece8a82e7435a3631aee
adricosmicfox <adriiphone@gmail.com>
2022-12-07 18:21:31 +0100

    basic layout for homeGuest

---
+++ resources/js/components/NavbarComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+
+        <div class="navbar navbar-expand-xxl bg-dark">
+
+            <!-- CAMBIARE E METTERE LOGO -->
+            <button type="button" class="btn btn-primary">logo deliveroo da cambiare</button>
+
+            <div>
+
+                <!-- MENU A TENDINA DA INSERIRE -->
+                <button type="button" class="btn btn-primary">Collabora con noi</button>
+
+                <!-- ROTTA D'ACCESSO DA INSERIRE -->
+                <button type="button" class="btn btn-primary">Registrati o accedi</button>
+
+                <!-- MENU A COMPARSA LATERALE DA INSERIRE -->
+                <button type="button" class="btn btn-primary">Menu</button>
+             </div>
+
+        </div>
+
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+
+<style scoped lang="scss">
+
+
+</style>
+
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
 
-<ExampleComponent/>
+<NavbarComponent/>
+<JumboComponent/>
+<CenterComponent/>
+<PartnerComponent/>
+<FooterComponent/>
 
   </div>
 </template>
 
 <script>
-import ExampleComponent from '../components/ExampleComponent.vue';
+import NavbarComponent from '../components/NavbarComponent.vue';
+import JumboComponent from '../components/JumboComponent.vue';
+import CenterComponent from '../components/CenterComponent.vue';
+import PartnerComponent from '../components/PartnerComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+
 
 export default {
 name:"App",
-components: {ExampleComponent}
+components: { NavbarComponent, JumboComponent, CenterComponent, PartnerComponent, FooterComponent }
 
 }
 </script>
