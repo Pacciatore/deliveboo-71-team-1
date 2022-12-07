2d9d8fd92db6f94842e3accffbeff934a71ed1fb
adricosmicfox <adriiphone@gmail.com>
2022-12-07 12:57:30 +0100

    init

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{any?}', function () {
+    return redirect()->route('guest.home');
+})->where('any', '.*');
