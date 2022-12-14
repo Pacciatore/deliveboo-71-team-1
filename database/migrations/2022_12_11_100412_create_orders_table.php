4f381c8bbf6dfb0de417b34698274c380d00c14e
Rosario Nici <rosarionici95@gmail.com>
2022-12-14 12:12:01 +0100

    add ordes dashboard

--- database/migrations/2022_12_11_100412_create_orders_table.php
+++ database/migrations/2022_12_11_100412_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOrderTable extends Migration
      */
     public function up()
     {
-        Schema::create('order', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
 
             $table->string('nome', 25);
---
+++ resources/views/guests/order.blade.php
@@ -0,0 +1,1 @@
+<h2>da implememntare lato front end</h2>
