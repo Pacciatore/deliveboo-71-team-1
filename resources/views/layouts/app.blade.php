6ab908396cbeccf9c5d4e10032fe1d3e460e51f3
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-15 02:45:03 +0100

    adeguamento layout.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('admin.home') }}">
+                                    <a class="dropdown-item" href="{{ route('admin.index') }}">
                                         Home
                                     </a>
 
