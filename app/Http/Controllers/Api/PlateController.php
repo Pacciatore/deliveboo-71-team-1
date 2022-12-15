8f49341d59cb3b3b655aaa6adf31b378104d09bd
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-15 22:58:49 +0100

    definizione index e show plate

---
+++ app/Http/Controllers/Api/PlateController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Plate;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class PlateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        try {
+            $plates = Plate::where('id', '>', 0)->paginate(15);
+            $data = [
+                'results' => $plates,
+                'success' => count($plates) > 0
+            ];
+        } catch (\Exception $e) {
+            $data = [
+                'error' => $e->message,
+                'success' => false
+            ];
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Plate  $plate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Plate $plate)
+    {
+        //
+        $data = [
+            'results' => $plate,
+            'success' => isset($plate)
+        ];
+
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Plate  $plate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Plate $plate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Plate  $plate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Plate $plate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Plate  $plate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Plate $plate)
+    {
+        //
+    }
+}
