0dff240ce6f60264423de89b087f12c7d9fa41e3
antoinde <antonello.indelicato98@gmail.com>
2022-12-16 12:50:21 +0100

    Creata tabella ponte e relazione order_plate

--- app/Order.php
+++ app/Order.php
@@ -8,4 +8,10 @@ class Order extends Model
 {
     //
     protected $fillable=[ 'ui_name', 'ui_email', 'address', 'payment', 'order_price' ];
+    
+    
+    public function plates(){
+        
+        return $this->belongsToMany('App\Plate');
+    }
 }
