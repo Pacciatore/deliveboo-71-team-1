447dd315d2e11d1b2880a57028fa6d4a8bba76f1
antoinde <antonello.indelicato98@gmail.com>
2022-12-16 13:30:19 +0100

    Migliorata estetica della view index di orders

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>INDEX</h1>
-    <h2>Ordini:</h2>
+    <div class="container clearfix">
+        <h1>GESTIONE DEGLI ORDINI</h1>
 
-    @foreach ($orders as $order)
-        <div style="border: 1px solid black;">
-            <div>Ordine numero:
-                <strong>{{ $order->id }}</strong>
+        @foreach ($orders as $order)
+            <div class="my-2 border rounded p-2">
+                <div>Ordine numero:
+                    <strong>{{ $order->id }}</strong>
+                </div>
+                <div>Ordine effettuato da:
+                    <strong>{{ $order->ui_name }}</strong>
+                </div>
+                <div>Indirizzo email associata all'ordine:
+                    <strong>{{ $order->ui_email }}</strong>
+                </div>
+                <div>Indirizzo di consegna dell'ordine:
+                    <strong>{{ $order->address }}</strong>
+                </div>
+                <div>Pagamento:
+                    <strong>
+                        @if ($order->payment)
+                            Pagamento effettuato!
+                        @else
+                            In attesa di pagamento!
+                        @endif
+                    </strong>
+                </div>
+                <div>Totale dell'ordine:
+                    <strong>{{ $order->order_price }} &euro;</strong>
+                </div>
             </div>
-            <div>Ordine effettuato da:
-                <strong>{{ $order->ui_name }}</strong>
-            </div>
-            <div>Indirizzo email associata all'ordine:
-                <strong>{{ $order->ui_email }}</strong>
-            </div>
-            <div>Indirizzo di consegna dell'ordine:
-                <strong>{{ $order->address }}</strong>
-            </div>
-            <div>Pagamento:
-                <strong>
-                    @if ($order->payment)
-                        Pagamento effettuato!
-                    @else
-                        In attesa di pagamento!
-                    @endif
-                </strong>
-            </div>
-            <div>Totale dell'ordine:
-                <strong>{{ $order->order_price }} &euro;</strong>
-            </div>
-        </div>
-    @endforeach
+        @endforeach
 
-    {{-- <a href="{{ route('admin.orders.create') }}">Crea un nuovo ordine!</a> --}}
+        {{-- <a href="{{ route('admin.orders.create') }}">Crea un nuovo ordine!</a> --}}
+    </div>
 @endsection
