5fe3d7312b428f2867a79887087d467530c20c8a
Rosario Nici <rosarionici95@gmail.com>
2022-12-20 08:54:53 +0100

    add restaurant page front layout

---
+++ resources/js/components/NavbarrestaurantComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+    <nav class="navbar bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="https://picsum.photos/200" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">Menu</button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <button>Registrati o Accedi</button>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    </a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">FAQ</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                </ul>
+                <form class="d-flex mt-3" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </nav>
+
+    </div>
+</template>
+
+<script>
+export default {
+name: 'NavRestaurant'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/pages/Restaurant.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <navbarrestaurant-component></navbarrestaurant-component>
+    </div>
+</template>
+
+<script>bar
+
+import NavbarrestaurantComponent from '../../js/components/NavbarrestaurantComponent.vue'
+
+
+
+export default {
+  name: 'Restaurant',
+  components:{
+    NavbarrestaurantComponent
+
+  }
+}
+</script>
+
+<style>
+
+</style>
