784d540c09b59808d2d47ab62f1c3959a87817a9
Rosario Nici <rosarionici95@gmail.com>
2022-12-20 09:55:28 +0100

    add parto of restaurant details

--- resources/js/components/NavbarrestaurantComponent.vue
+++ resources/js/components/NavbarrestaurantComponent.vue
@@ -8,30 +8,20 @@
             </button>
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Deliverboo</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">Menu</button>
             </div>
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <button>Registrati o Accedi</button>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-
-                    </a>
-                    <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">FAQ</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item">
+                        <button>Registrati o Accedi</button>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">FAQ</a>
+                    </li>
                 </ul>
                 <form class="d-flex mt-3" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -47,10 +37,11 @@
 
 <script>
 export default {
-name: 'NavRestaurant'
+name: 'NavbarrestaurantComponent'
 }
 </script>
 
 <style>
+
 
 </style>
--- resources/js/pages/Restaurant.vue
+++ resources/js/pages/Restaurant.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
         <navbarrestaurant-component></navbarrestaurant-component>
+        <restaurantdetails-component></restaurantdetails-component>
     </div>
 </template>
 
 <script>bar
 
 import NavbarrestaurantComponent from '../../js/components/NavbarrestaurantComponent.vue'
-
+import RestaurantdetailsComponent from '../../js/components/RestaurantdetailsComponent.vue'
 
 
 export default {
   name: 'Restaurant',
   components:{
-    NavbarrestaurantComponent
+    NavbarrestaurantComponent,
+    RestaurantdetailsComponent
 
   }
 }
