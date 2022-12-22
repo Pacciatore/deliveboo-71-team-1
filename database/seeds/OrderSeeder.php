e1708e4a782ec3526266d320c922e319ae595ec4
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-12-22 20:46:57 +0100

    orderSeeder con fk a plates

--- database/seeds/OrderSeeder.php
+++ database/seeds/OrderSeeder.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use App\Order;
 use Faker\Generator as Faker;
+// use Illuminate\Support\Facades\DB;
+
+
+use App\Plate;
 
 class OrderSeeder extends Seeder
 {
@@ -14,17 +18,56 @@ class OrderSeeder extends Seeder
     public function run(Faker $faker)
     {
         //
-        $MAX=25;
-        for( $i = 0; $i < $MAX; $i++ ){
+        $MAX = 25;
+        for ($i = 0; $i < $MAX; $i++) {
 
             $order = new Order();
-            $order->ui_name = $faker->firstName($gender = null) . ' ' . $faker->lastName() ;
+            $order->ui_name = $faker->firstName($gender = null) . ' ' . $faker->lastName();
             $order->ui_email = $faker->safeEmail();
             $order->address = $faker->streetName() . ' n° ' . $faker->buildingNumber() . ', ' . $faker->city();
             $order->payment = $faker->boolean();
             $order->order_price = $faker->randomFloat(2, 1, 200);
 
             $order->save();
+
+            $platesArray = $faker->randomNumber(2, true);
+            $order->plates()->sync($platesArray);
         }
     }
+
+
+    // private function getPlates($faker)
+    // {
+    //     $platesArray = [];
+
+    //     // Ottengo un id random di ristoratore
+    //     $user_id = $faker->numberBetween(1, 25);
+
+    //     // Prendo tutti i piatti relativi al ristoratore
+    //     $plates = DB::table('plates')->where('user_id', $user_id)->pluck('id')->first();
+    //     $platesCounter = DB::table('plates')->where('user_id', $user_id)->count();
+
+    //     // Ottengo un numero casuale tra uno ed il numero di piatti del ristorante
+    //     $platesOrderNumber = $faker->numberBetween(1, $platesCounter);
+
+    //     // Itera per il numero casuale di piatti presente nell'ordine
+    //     for ($i = 0; $i < $platesOrderNumber; $i++) {
+
+    //         // Ottiene un piatto casuale tra quelli trovati
+    //         $randomPlate = $faker->randomElement($plates);
+
+    //         // Se non è doppione viene aggiunto, se è doppione diminuisce il contatore
+    //         if (!in_array($randomPlate, $platesArray)) {
+    //             $platesArray[] = $randomPlate;
+    //         } else {
+    //             $platesArray = 1;
+    //         }
+    //         // Commentato perché potrebbe generare un loop
+    //         // else {
+    //         //     $i--;
+    //         // }
+    //     }
+
+    //     return $platesArray == [] ? 1 : $platesArray;
+    // }
 }
--- public/js/front.js
+++ public/js/front.js
@@ -63948,8 +63948,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/deliveboo-71-team-1/resources/js/front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/deliveboo-71-team-1/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\raimo\Desktop\Boolean\Corso\ProgettoFinale\deliveboo-71-team-1\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\raimo\Desktop\Boolean\Corso\ProgettoFinale\deliveboo-71-team-1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
